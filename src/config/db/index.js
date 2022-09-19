e3b30cf49962a1940190ff07a547833719c2199b
Your Name <you@example.com>
2022-09-19 18:16:40 +0700

    three commit

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose')
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb+srv://The529:The529@cluster0.4rw2ops.mongodb.net/?retryWrites=true&w=majority');
+        await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://The529:The529@cluster0.4rw2ops.mongodb.net/?retryWrites=true&w=majority' , {
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        });
         console.log('connect seccessfully')
     } catch (error) {
         console.log('connect fail')
